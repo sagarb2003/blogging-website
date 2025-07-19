42c5a27e35758b09904842752e6c43e6f224240d
sagarb2003 <sagarssb2003@gmail.com>
2025-07-19 12:39:17 +0530

    feat(auth): add authentication check to blog pages

--- frontend/pages/Blog.tsx
+++ frontend/pages/Blog.tsx
@@ -1,12 +1,19 @@
 // import React from "react";
 import { useBlog } from "../hooks/useBlog";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { FullBlog } from "../components/FullBlog";
 import { Skeleton } from "../components/Skeleton";
+import { useEffect } from "react";
 
 export const Blog = () => {
   const { id } = useParams();
   const { blog, loading } = useBlog({ id: id || "" });
+  const navigate= useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/signin");
+    }
+  }, []);
 
   if (loading) {
     return (
--- frontend/pages/CreateBlog.tsx
+++ frontend/pages/CreateBlog.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { format } from 'date-fns';
@@ -17,6 +17,11 @@ export const CreateBlog = () => {
     thumbnail: "",
     publishedDate: "",
   });
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/signin");
+    }
+  }, []);
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
